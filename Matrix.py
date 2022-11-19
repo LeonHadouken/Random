86eb3b2ee8eb6da367cb99afa40b96621f0a9219
LeonHadouken <Leonhadouken@gmail.com>
2022-11-19 03:28:20 +0300

    Ну тут я максимально пытался доступно изложить

--- Matrix.py
+++ Matrix.py
@@ -1,23 +1,17 @@
-row = int(input('Сколько строк в таблице?'))
-column = int(input('Сколько столбцов в таблице?'))
-# Создаем пустую матрицу
-map = []
-# Для каждой строки в кол-ве (x)
-for i in range(row):
-    # Добавляем список с кол-вом столбцов равным (y)
-    map.append([0] * column)
-# Выводим матрицу построчно
-for row in map:
-    for column in row:
-        print(column, end=' ')
+rows = int(input('Сколько строк в таблице?'))
+columns = int(input('Сколько столбцов в таблице?'))
+map = []  # Создаем пустую матрицу
+for i in range(rows):
+    map.append([0] * columns)
+for rows in map:  # Выводим матрицу построчно
+    for columns in rows:
+        print(columns, end=' ')
     print()
-row = int(input('Какую строку выбрать?'))
-column = int(input('Какой столбец?'))
+rows = int(input('Какую строку выбрать?'))
+columns = int(input('Какой столбец?'))
 change = int(input('На что заменить?'))
-# Присваиваем измененное значение в ячейку по индексу, исходя из введенных данных
-map[row - 1][column - 1] = change
-# Снова выводим карту построчно
-for i in map:
-    for j in i:
-        print(j, end=' ')
+map[rows - 1][columns - 1] = change
+for rows in map:  # Снова выводим карту построчно
+    for columns in rows:
+        print(columns, end=' ')
     print()
