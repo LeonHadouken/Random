45a2bcfc70ff45d36cfe44ba6c53bcea86b359dd
LeonHadouken <Leonhadouken@gmail.com>
2022-11-19 04:09:03 +0300

    v 1.1

--- Counter.py
+++ Counter.py
@@ -1,13 +1,13 @@
-count_digits = 0  # счетчик цифр в числе
-summary = 0  # сумма всех цифр в числе
-multiply = 1  # произведение всех цифр в числе
-maximum_digit = 0  # наибольшая цифра в числе
-minimum_digit = 9  # наименьшая цифры в числе
+count_digits = 0
+summary = 0
+multiply = 1
+maximum_digit = 0
+minimum_digit = 9
 number = int(input('Введите число'))
 saved_number = number
 
 while number > 0:
-    last_digit = x % 10
+    last_digit = number % 10
     count_digits += 1
     summary += last_digit
     multiply *= last_digit
@@ -15,9 +15,9 @@ while number > 0:
         maximum_digit = last_digit
     if last_digit < minimum_digit:
         minimum_digit = last_digit
-    x = x // 10  # Отделить последнюю цифру от числа.
+    number = number // 10  # Отделить последнюю цифру от числа.
 
-print(f'кол-во цифр в числе {saved_number} = {count}')
+print(f'кол-во цифр в числе {saved_number} = {count_digits}')
 print(f'сумма цифр в числе {saved_number} = {summary}')
 print(f'произведение цифр в числе {saved_number} = {multiply}')
 print(f'наибольшая цифра {maximum_digit}')
