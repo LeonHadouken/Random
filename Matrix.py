ffb3546f901597931d5d3a378198a986eb5c0977
LeonHadouken <Leonhadouken@gmail.com>
2022-11-19 03:07:03 +0300

    Теперь еще понятнее

--- Matrix.py
+++ Matrix.py
@@ -1,21 +1,23 @@
-x = int(input('Сколько строк в таблице?'))
-y = int(input('Сколько столбцов в таблице?'))
+row = int(input('Сколько строк в таблице?'))
+column = int(input('Сколько столбцов в таблице?'))
 # Создаем пустую матрицу
 map = []
 # Для каждой строки в кол-ве (x)
-for i in range(x):
-    # Добавляем список с кол-вом координат равным (y)
-    map.append([0] * y)
+for i in range(row):
+    # Добавляем список с кол-вом столбцов равным (y)
+    map.append([0] * column)
 # Выводим матрицу построчно
-for i in map:
-    print(i)
-x = int(input('Какую строку выбрать?'))
-y = int(input('Какой столбец?'))
+for row in map:
+    for column in row:
+        print(column, end=' ')
+    print()
+row = int(input('Какую строку выбрать?'))
+column = int(input('Какой столбец?'))
 change = int(input('На что заменить?'))
 # Присваиваем измененное значение в ячейку по индексу, исходя из введенных данных
-map[x - 1][y - 1] = change
+map[row - 1][column - 1] = change
 # Снова выводим карту построчно
 for i in map:
-    print(i)
-
-
+    for j in i:
+        print(j, end=' ')
+    print()
