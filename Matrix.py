67e9d3c402f44bdfcaacada8bef2e116f2c5da2c
LeonHadouken <Leonhadouken@gmail.com>
2022-11-19 17:58:20 +0300

    Создание и изменение таблиц

--- Matrix.py
+++ Matrix.py
@@ -9,12 +9,13 @@ for row in map:  # Выводим таблицу построчно
     print()
 
 # Также скрипт позволяет заменить ячейку в таблице по ее координатам
+# Поскольку счет по индексам идет с коэффицента 0, из каждой из координат необходимо вычесть единицу
 
 row_number = int(input('Какую строку выбрать?'))
 column_number = int(input('Какой столбец?'))
 change = int(input('На что заменить?'))
 map[row_number - 1][column_number - 1] = change
-for row in map:  # Выводим таблицу построчно
+for row in map:  # Выводим измененную таблицу построчно
     for column in row:
         print(column, end=' ')
     print()
